665eea0be40fcef6f517c837d904d574371e9d61
aagarwal19 <aagarwal19@DESKTOP-6M0P9SH.fios-router.home>
2018-09-25 19:11:14 -0400

    Hopefully Tally Counter work done at home, with homework

--- TallyCounter.java
+++ TallyCounter.java
@@ -1,4 +1,47 @@
-
-public class TallyCounter {
-
+/**
+ * 
+ * @author Arun Agarwal Pd. 7
+ *
+ */
+public class TallyCounter 
+{
+	// Instance variable
+	
+	private int count; 
+	//Constructor: starting at 0; no return type and same name as class 
+	
+	public TalleyCounter() 
+	{ 
+		count = 0;
+	}
+	//Constructor: starting at any number of choice 
+	
+	public TalleyCounter(int startNum) 
+	{ 
+		count = startNum; 
+	} 
+	//Constructor: clicker 
+	
+	public void click() 
+	{ 
+		count = count + 1;
+		// could also do count++
+		//could also do count+=1
+		//could also do count+=37
+		//could also do count-- to subtract one
+		//could also do count *5 to multiply count by 5
+	}
+	public void reset()
+	{
+		if(count>999)
+		count=0;
+	}
+	public int getCount()
+	{
+		return count;
+	}
+	public void unclick()
+	{
+		count = count - 1;
+	}
 }
